56ee90c59a4ab88fb79615931ab4bbe7d37dcb27
SirPoz <nicolas@pozdena.at>
2023-11-13 13:10:26 +0100

    added QueuePayload class and according validator

---
+++ SWKOM-paperless.BusinessLogic.Entities/QueuePayload.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWKOM_paperless.BusinessLogic.Entities
+{
+    public class QueuePayload
+    {
+        public string bucket { get; set; }
+        public string filename { get; set; }
+
+        public QueuePayload(string bucket, string filename)
+        {
+            this.bucket = bucket;
+            this.filename = filename;
+        }
+    }
+}
