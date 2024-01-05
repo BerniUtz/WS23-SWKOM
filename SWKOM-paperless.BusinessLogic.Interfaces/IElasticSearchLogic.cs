d736948ba16b20d4723a56bbbe58e008a23c99e0
Bernhard Utz <if21b241@technikum-wien.at>
2024-01-05 19:28:23 +0100

    implementation of elastic search

--- SWKOM-paperless.BusinessLogic.Interfaces/IElasticSearchLogic.cs
+++ SWKOM-paperless.BusinessLogic.Interfaces/IElasticSearchLogic.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SWKOM_paperless.BusinessLogic.Entities;
 
 namespace SWKOM_paperless.BusinessLogic.Interfaces
 {
     public interface IElasticSearchLogic
     {
-        
+        public Task AddDocumentAsync(Document document);
+        public Task<List<Document>> SearchDocumentsAsync(string query, string indexName);
+
     }
 }
---
+++ SWKOM-paperless.BusinessLogic/ElasticSearchService.cs
@@ -0,0 +1,77 @@
+using Elasticsearch.Net;
+using log4net;
+using Nest;
+using SWKOM_paperless.BusinessLogic.Entities;
+using SWKOM_paperless.BusinessLogic.Interfaces;
+
+namespace SWKOM_paperless.BusinessLogic;
+
+public class ElasticSearchService : IElasticSearchLogic
+{
+    private static readonly ILog _logger = LogManager.GetLogger(typeof(ElasticSearchService));
+    private readonly IElasticClient _elasticClient;
+    
+    public ElasticSearchService()
+    {
+        var settings = new ConnectionSettings(new Uri("https://localhost:9200"))
+            .ServerCertificateValidationCallback((sender, certificate, chain, sslPolicyErrors) => true)
+            .DefaultIndex("documents")
+            .BasicAuthentication("elastic", "password"); // TODO: Move to config file
+        _elasticClient = new ElasticClient(settings);
+    }
+    public ElasticSearchService(IElasticClient elasticClient)
+    {
+        _elasticClient = elasticClient;
+    }
+    
+    public async Task AddDocumentAsync(Document document)
+    {
+        var indexResponse = await _elasticClient.IndexAsync(document, idx => idx
+                .Index("documents")
+                .Id(document.Id) // Specify the document ID
+                .Refresh(Refresh.WaitFor) // Specify refresh behavior after indexing
+        );
+
+        if (!indexResponse.IsValid)
+        {
+            _logger.Error($"Failed to index document: {indexResponse.DebugInformation}");
+        }
+        else
+        {
+            _logger.Info($"Document indexed: {indexResponse.Id}");
+        }
+    }
+    public async Task<List<Document>> SearchDocumentsAsync(string query, string indexName)
+    {
+        var response = await _elasticClient.SearchAsync<Document>(s => s
+            .Index(indexName)
+            .Query(q => q
+                    .Match(m => m
+                        .Field(f => f.Title)
+                        .Query(query)
+                    ) || q
+                    .Match(m => m
+                        .Field(f => f.Content)
+                        .Query(query)
+                    )
+            )
+        );
+
+        if (response.IsValid)
+        {
+            _logger.Info($"Found {response.Documents.Count} documents for query: {query}");
+            return response.Documents.ToList();
+        }
+        else if (response.ServerError != null)
+        {
+            _logger.Error($"Server error: {response.ServerError.Error}");
+        }
+        else
+        {
+            _logger.Info($"No documents found for query: {query}");
+        }
+
+        return new List<Document>();
+    }
+
+}
