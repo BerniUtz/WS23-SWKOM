4c7af512b46bee324815d6fc61f9e799096e8af2
Leonard Struck <me@leonardstruck.com>
2024-01-09 22:16:53 +0100

    run the ocr service in a docker container

--- SWKOM-paperless.OCRWorker/Program.cs
+++ SWKOM-paperless.OCRWorker/Program.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Minio;
 using SWKOM_paperless.BusinessLogic;
-using SWKOM_paperless.OCRWorker;
 using SWKOM_paperless.ServiceAgents;
-using SWKOM_paperless.ServiceAgents.Interfaces;
-using System.Configuration;
 
 namespace SWKOM_paperless.OCRWorker
 {
@@ -12,9 +8,15 @@ namespace SWKOM_paperless.OCRWorker
     {
         static void Main()
         {
+            // check if the Environment Variable ENVIRONMENT is set to docker
+            // if so, use the OCRWorkerSettings.docker.json file
+            // otherwise use the OCRWorkerSettings.json file
+            var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+            var settingsFile = environment == "docker" ? "OCRWorkerSettings.docker.json" : "OCRWorkerSettings.json";
+            
             IConfiguration config = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("OCRWorkerSettings.json")
+            .AddJsonFile(settingsFile, optional: false, reloadOnChange: true)
             .Build();
             
             var queueOptions = config.GetSection("RabbitMQ").Get<RabbitMQOptions>();
